1cd677087688732e0a0f31b26b2b47012d1901d2
Ryszard Mochalski <138623138+rysz99@users.noreply.github.com>
2025-04-28 20:54:22 +0200

    Add files via upload

---
+++ parks.js
@@ -0,0 +1,232 @@
+var numer = Math.floor(Math.random()*5)+1;
+
+
+function pieninski() 
+{
+    numer++; if (numer>5) numer = 1;
+    var plik = "<img src=\"pieninski" +numer+ ".jpeg\" />";    
+    
+    document.getElementById("s1").innerHTML = plik;
+    
+    setTimeout("pieninski()",4000);    
+}
+
+function ojcowski() 
+{
+    numer++; if (numer>5) numer = 1;
+    var plik = "<img src=\"ojcowski" +numer+ ".jpeg\" />";    
+    
+    document.getElementById("s2").innerHTML = plik;
+    
+    setTimeout("ojcowski()",4000);    
+}
+
+function babiogorski() 
+{
+    numer++; if (numer>5) numer = 1;
+    var plik = "<img src=\"babiogorski" +numer+ ".jpeg\" />";    
+    
+    document.getElementById("s3").innerHTML = plik;
+    
+    setTimeout("babiogorski()",4000);    
+}
+
+function bieszczadzki() 
+{
+    numer++; if (numer>5) numer = 1;
+    var plik = "<img src=\"bieszczadzki" +numer+ ".jpeg\" />";    
+    
+    document.getElementById("s4").innerHTML = plik;
+    
+    setTimeout("bieszczadzki()",4000);    
+}
+
+function drawienski() 
+{
+    numer++; if (numer>5) numer = 1;
+    var plik = "<img src=\"drawienski" +numer+ ".jpeg\" />";    
+    
+    document.getElementById("s5").innerHTML = plik;
+    
+    setTimeout("drawienski()",4000);    
+}
+
+function gor_stolowych() 
+{
+    numer++; if (numer>5) numer = 1;
+    var plik = "<img src=\"gor_stolowych" +numer+ ".jpeg\" />";    
+    
+    document.getElementById("s6").innerHTML = plik;
+    
+    setTimeout("gor_stolowych()",4000);    
+}
+
+function karkonoski() 
+{
+    numer++; if (numer>5) numer = 1;
+    var plik = "<img src=\"karkonoski" +numer+ ".jpeg\" />";    
+    
+    document.getElementById("s7").innerHTML = plik;
+    
+    setTimeout("karkonoski()",4000);    
+}
+
+function narwianski() 
+{
+    numer++; if (numer>5) numer = 1;
+    var plik = "<img src=\"narwianski" +numer+ ".jpeg\" />";    
+    
+    document.getElementById("s8").innerHTML = plik;
+    
+    setTimeout("narwianski()",4000);    
+}
+
+function roztoczanski() 
+{
+    numer++; if (numer>5) numer = 1;
+    var plik = "<img src=\"roztoczanski" +numer+ ".jpeg\" />";    
+    
+    document.getElementById("s9").innerHTML = plik;
+    
+    setTimeout("roztoczanski()",4000);    
+}
+
+function swietokrzyski() 
+{
+    numer++; if (numer>5) numer = 1;
+    var plik = "<img src=\"swietokrzyski" +numer+ ".jpeg\" />";    
+    
+    document.getElementById("s10").innerHTML = plik;
+    
+    setTimeout("swietokrzyski()",4000);    
+}
+
+function wigierski() 
+{
+    numer++; if (numer>5) numer = 1;
+    var plik = "<img src=\"wigierski" +numer+ ".jpeg\" />";    
+    
+    document.getElementById("s11").innerHTML = plik;
+    
+    setTimeout("wigierski()",4000);    
+}
+
+function ujscie_warty() 
+{
+    numer++; if (numer>5) numer = 1;
+    var plik = "<img src=\"ujscie_warty" +numer+ ".jpeg\" />";    
+    
+    document.getElementById("s12").innerHTML = plik;
+    
+    setTimeout("ujscie_warty()",4000);    
+}
+
+function biebrzanski() 
+{
+    numer++; if (numer>5) numer = 1;
+    var plik = "<img src=\"biebrzanski" +numer+ ".jpeg\" />";    
+    
+    document.getElementById("s13").innerHTML = plik;
+    
+    setTimeout("biebrzanski()",4000);    
+}
+
+function bialowieski() 
+{
+    numer++; if (numer>5) numer = 1;
+    var plik = "<img src=\"bialowieski" +numer+ ".jpeg\" />";    
+    
+    document.getElementById("s14").innerHTML = plik;
+    
+    setTimeout("bialowieski()",4000);    
+}
+
+function bory_tucholskie() 
+{
+    numer++; if (numer>5) numer = 1;
+    var plik = "<img src=\"bory_tucholskie" +numer+ ".jpeg\" />";    
+    
+    document.getElementById("s15").innerHTML = plik;
+    
+    setTimeout("bory_tucholskie()",4000);    
+}
+
+function gorczanski() 
+{
+    numer++; if (numer>5) numer = 1;
+    var plik = "<img src=\"gorczanski" +numer+ ".jpeg\" />";    
+    
+    document.getElementById("s16").innerHTML = plik;
+    
+    setTimeout("gorczanski()",4000);    
+}
+
+function kampinoski() 
+{
+    numer++; if (numer>5) numer = 1;
+    var plik = "<img src=\"kampinoski" +numer+ ".jpeg\" />";    
+    
+    document.getElementById("s17").innerHTML = plik;
+    
+    setTimeout("kampinoski()",4000);    
+}
+
+function magurski() 
+{
+    numer++; if (numer>5) numer = 1;
+    var plik = "<img src=\"magurski" +numer+ ".jpeg\" />";    
+    
+    document.getElementById("s18").innerHTML = plik;
+    
+    setTimeout("magurski()",4000);    
+}
+
+function poleski() 
+{
+    numer++; if (numer>5) numer = 1;
+    var plik = "<img src=\"poleski" +numer+ ".jpeg\" />";    
+    
+    document.getElementById("s19").innerHTML = plik;
+    
+    setTimeout("poleski()",4000);    
+}
+
+function slowinski() 
+{
+    numer++; if (numer>5) numer = 1;
+    var plik = "<img src=\"slowinski" +numer+ ".jpeg\" />";    
+    
+    document.getElementById("s20").innerHTML = plik;
+    
+    setTimeout("slowinski()",4000);    
+}
+
+function tatrzanski() 
+{
+    numer++; if (numer>5) numer = 1;
+    var plik = "<img src=\"tatrzanski" +numer+ ".jpeg\" />";    
+    
+    document.getElementById("s21").innerHTML = plik;
+    
+    setTimeout("tatrzanski()",4000);    
+}
+
+function wielkopolski() 
+{
+    numer++; if (numer>5) numer = 1;
+    var plik = "<img src=\"wielkopolski" +numer+ ".jpeg\" />";    
+    
+    document.getElementById("s22").innerHTML = plik;
+    
+    setTimeout("wielkopolski()",4000);    
+}
+
+function wolinski() 
+{
+    numer++; if (numer>5) numer = 1;
+    var plik = "<img src=\"wolinski" +numer+ ".jpeg\" />";    
+    
+    document.getElementById("s23").innerHTML = plik;
+    
+    setTimeout("wolinski()",4000);    
+}
